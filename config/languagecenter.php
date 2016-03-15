923a66887ad505d5d68f0d117feef76c7be63faa
Mark Topper <mail@webman.io>
2016-03-15 10:05:10 -0400

    Applied fixes from StyleCI

--- config/languagecenter.php
+++ config/languagecenter.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    
+
     'url' => env('LANGUAGE_CENTER_URL'),
-    
+
     'username' => env('LANGUAGE_CENTER_USERNAME'),
-    
+
     'password' => env('LANGUAGE_CENTER_PASSWORD'),
-    
+
     'platform' => env('LANGUAGE_CENTER_PLATFORM'),
 
 ];
